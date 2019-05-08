ff4a9efafe65f919ee5bbec0441aec92dce779e1
koppepan <koppepan0114@gmail.com>
2019-05-08 12:52:07 +0900

    Buttonの機能を拡張したクラスを作成
    ダブルクリック

---
+++ Assets/Components/Editor/UI/ButtonExEditor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.UI;
+
+namespace Components.UI
+{
+	[CustomEditor(typeof(ButtonEx), true)]
+	[CanEditMultipleObjects]
+	public class ButtonExEditor : ButtonEditor
+	{
+		SerializedProperty activateDoubleClickProperty;
+		SerializedProperty onDoubleClickProperty;
+		
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			activateDoubleClickProperty = serializedObject.FindProperty("activateDoubleClick");
+			onDoubleClickProperty = serializedObject.FindProperty("doubleClick");
+		}
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			serializedObject.Update();
+			EditorGUILayout.PropertyField(activateDoubleClickProperty);
+			if (activateDoubleClickProperty.boolValue)
+			{
+				EditorGUILayout.PropertyField(onDoubleClickProperty);
+			}
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
---
+++ Assets/Components/UI/ButtonEx.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+using UnityEngine.EventSystems;
+
+namespace Components.UI
+{
+	[AddComponentMenu("UI/ButtonEx", 30)]
+	public class ButtonEx : Button
+	{
+		private const float DoubleClickInterval = 0.3f;
+
+		[System.Serializable]
+		public class ButtonDoubleClickEvent : UnityEvent { }
+
+		[FormerlySerializedAs("onDoubleClick")]
+		[SerializeField]
+		ButtonDoubleClickEvent doubleClick = new ButtonDoubleClickEvent();
+
+		public ButtonDoubleClickEvent onDoubleClick
+		{
+			get { return doubleClick; }
+		}
+
+		[SerializeField]
+		private bool activateDoubleClick = false;
+
+		private float lastClickTime;
+		private Coroutine clickWaitCoroutine;
+
+		public override void OnPointerClick(PointerEventData eventData)
+		{
+			if (eventData.button != PointerEventData.InputButton.Left)
+			{
+				return;
+			}
+			if (!IsActive() || !IsInteractable())
+			{
+				return;
+			}
+
+			if (!activateDoubleClick)
+			{
+				Click();
+				return;
+			}
+
+			if (Time.time - lastClickTime < DoubleClickInterval)
+			{
+				DoubleClick();
+			}
+			else
+			{
+				clickWaitCoroutine = StartCoroutine(Wait(DoubleClickInterval, Click));
+			}
+			lastClickTime = Time.time;
+		}
+
+		IEnumerator Wait(float time, System.Action complete)
+		{
+			yield return new WaitForSeconds(time);
+			complete?.Invoke();
+		}
+
+		private void Click()
+		{
+			lastClickTime = 0;
+			UISystemProfilerApi.AddMarker("Button.onClick", this);
+			onClick.Invoke();
+		}
+
+		private void DoubleClick()
+		{
+			if (clickWaitCoroutine != null)
+			{
+				StopCoroutine(clickWaitCoroutine);
+				clickWaitCoroutine = null;
+			}
+
+			lastClickTime = 0;
+			UISystemProfilerApi.AddMarker("Button.onDoubleClick", this);
+			onDoubleClick.Invoke();
+		}
+	}
+}
