b88f2959c5fe18a1a2fda04bb56983712d65e3bc
koppepan <koppepan0114@gmail.com>
2019-05-09 17:23:01 +0900

    SubLayerTypeのEnumを設定するためのScriptableObject

---
+++ Assets/Flour/UI/Layer/SubLayerSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flour.UI
+{
+	[CreateAssetMenu]
+	public class SubLayerSetting : ScriptableObject
+	{
+		[System.Serializable]
+		public struct SubLayer
+		{
+			public string typeName;
+			public string srcPath;
+		}
+
+		public string exportPath;
+		public List<SubLayer> settings = new List<SubLayer>();
+	}
+}
