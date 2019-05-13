9b10f858e7b33b43aa41daf9142a4026c213cf97
koppepan <koppepan0114@gmail.com>
2019-05-13 12:15:36 +0900

    IniファイルをLoadする処理

---
+++ Assets/Flour/Common/IniFile.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Flour
+{
+	public class IniFile
+	{
+		private Regex RegexSection = new Regex(@"^\s*\[(?<section>[^\]]+)\].*$", RegexOptions.Singleline | RegexOptions.CultureInvariant);
+		private Regex RegexNameValue = new Regex(@"^\s*(?<name>[^=]+)=(?<value>.*?)(\s+;(?<comment>.*))?$", RegexOptions.Singleline | RegexOptions.CultureInvariant);
+
+		Dictionary<string, Dictionary<string, string>> sections = new Dictionary<string, Dictionary<string, string>>();
+		string currentSection = string.Empty;
+
+		public IniFile(string filePath)
+		{
+			sections.Add(currentSection, new Dictionary<string, string>());
+
+			using (var reader = new StreamReader(filePath))
+			{
+				while (!reader.EndOfStream)
+				{
+					ParseLine(reader.ReadLine());
+				}
+			}
+		}
+		public IniFile(string[] contents)
+		{
+			sections.Add(currentSection, new Dictionary<string, string>());
+			for (int i = 0; i < contents.Length; i++)
+			{
+				ParseLine(contents[i]);
+			}
+		}
+
+
+		void ParseLine(string line)
+		{
+			if (string.IsNullOrEmpty(line) || line.Length == 0)
+			{
+				return;
+			}
+			if (line.StartsWith(";", System.StringComparison.Ordinal))
+			{
+				return;
+			}
+
+			var matchName = RegexNameValue.Match(line);
+			if (matchName.Success)
+			{
+				sections[currentSection][matchName.Groups["name"].Value.Trim()] = matchName.Groups["value"].Value.Trim();
+				return;
+			}
+
+			var section = RegexSection.Match(line);
+			if (section.Success)
+			{
+				currentSection = section.Groups["section"].Value.Trim();
+
+				if (!sections.ContainsKey(currentSection))
+				{
+					sections.Add(currentSection, new Dictionary<string, string>());
+				}
+			}
+		}
+
+		public IEnumerable<string> GetSections()
+		{
+			return sections.Keys;
+		}
+		public IEnumerable<string> GetKeys(string section)
+		{
+			if (!sections.ContainsKey(section))
+			{
+				return Enumerable.Empty<string>();
+			}
+			return sections[section].Keys;
+		}
+		public Dictionary<string, string> GetContents(string section)
+		{
+			if (!sections.ContainsKey(section))
+			{
+				return new Dictionary<string, string>();
+			}
+			return sections[section];
+		}
+
+		public string GetValue(string section, string key)
+		{
+			if (!sections.ContainsKey(section) || !sections[section].ContainsKey(key))
+			{
+				return "";
+			}
+			return sections[section][key];
+		}
+		public bool GetBoolean(string section, string key)
+		{
+			var val = GetValue(section, key);
+			return bool.TryParse(val, out bool ret) ? ret : default;
+		}
+		public int GetInt(string section, string key)
+		{
+			var val = GetValue(section, key);
+			return int.TryParse(val, out int ret) ? ret : default;
+		}
+		public float GetFloat(string section, string key)
+		{
+			var val = GetValue(section, key);
+			return float.TryParse(val, out float ret) ? ret : default;
+		}
+		public double GetDouble(string section, string key)
+		{
+			var val = GetValue(section, key);
+			return double.TryParse(val, out double ret) ? ret : default;
+		}
+	}
+}
