022ee84b26001b45ee7354d4d17c5e2b81f25cbe
koppepan <koppepan0114@gmail.com>
2019-05-15 15:30:53 +0900

    DontDestroyObjectList

--- Assets/Flour/Scripts/Common/DontDestroyObjectList.cs
+++ Assets/Flour/Scripts/Common/DontDestroyObjectList.cs
@@ -1,44 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 
 namespace Flour
 {
 	static public class DontDestroyObjectList
 	{
-		static List<Tuple<Type, GameObject>> dontDestroyObjects = new List<Tuple<Type, GameObject>>();
+		static Dictionary<Type, GameObject> dontDestroyObjects = new Dictionary<Type, GameObject>();
 
 		public static void Add<T>(GameObject obj)
 		{
-			if (Contains<T>())
+			if(dontDestroyObjects.ContainsKey(typeof(T)))
 			{
 				UnityEngine.Object.Destroy(obj);
 				return;
 			}
 			UnityEngine.Object.DontDestroyOnLoad(obj);
-			dontDestroyObjects.Add(new Tuple<Type, GameObject>(typeof(T), obj));
+			dontDestroyObjects.Add(typeof(T), obj);
 		}
 
 		public static void Remove<T>()
 		{
-			if (!Contains<T>())
+			if(dontDestroyObjects.ContainsKey(typeof(T)))
 			{
 				return;
 			}
-			var item = dontDestroyObjects.First(x => x.Item1 == typeof(T));
-			dontDestroyObjects.Remove(item);
-			UnityEngine.Object.Destroy(item.Item2);
-		}
-
-		public static bool Contains<T>()
-		{
-			return dontDestroyObjects.Any(x => x.Item1 == typeof(T));
+			UnityEngine.Object.Destroy(dontDestroyObjects[typeof(T)]);
+			dontDestroyObjects.Remove(typeof(T));
 		}
 
 		public static void Clear()
 		{
-			dontDestroyObjects.ForEach(x => UnityEngine.Object.Destroy(x.Item2));
+			foreach (var obj in dontDestroyObjects.Values)
+			{
+				UnityEngine.Object.Destroy(obj);
+			}
 			dontDestroyObjects.Clear();
 		}
 	}
