72c3c61a0f1bbf8e55a5379131c0ffc4f1821e42
koppepan <koppepan0114@gmail.com>
2019-05-22 16:32:39 +0900

    SafeAreaの処理を修正

--- Assets/Flour/Scripts/Layer/Layer.cs
+++ Assets/Flour/Scripts/Layer/Layer.cs
@@ -25,29 +25,7 @@ namespace Flour.Layer
 
 		Transform GetContentsArea(Rect safeArea, Vector2 screenSize)
 		{
-#if UNITY_EDITOR
-			if (screenSize.x == 1125 && screenSize.y == 2436 || screenSize.x == 2436 && screenSize.y == 1125)
-			{
-				Vector2 positionOffset;
-				Vector2 sizeOffset;
-
-				//縦持ち
-				if (screenSize.x < screenSize.y)
-				{
-					positionOffset = new Vector2(0f, safeArea.size.y * 34f / 812f);
-					sizeOffset = positionOffset + new Vector2(0f, safeArea.size.y * 44f / 812f);
-				}
-				//横持ち
-				else
-				{
-					positionOffset = new Vector2(safeArea.size.x * 44f / 812f, safeArea.size.y * 21f / 375f);
-					sizeOffset = positionOffset + new Vector2(safeArea.size.x * 44f / 812f, 0f);
-				}
-
-				safeArea.position = safeArea.position + positionOffset;
-				safeArea.size = safeArea.size - positionOffset - sizeOffset;
-			}
-#endif
+			safeArea = SafeAreaSimulateData.GetSafeArea(screenSize, safeArea);
 
 			if (safeArea.size == screenSize)
 			{
@@ -57,19 +35,10 @@ namespace Flour.Layer
 			var rect = new GameObject("ContentsArea", typeof(RectTransform)).GetComponent<RectTransform>();
 			rect.SetParent(this.transform, false);
 
-			rect.anchoredPosition = Vector2.zero;
-			rect.sizeDelta = Vector2.zero;
-
-			var min = safeArea.position;
-			var max = safeArea.position + safeArea.size;
-
-			min.x /= screenSize.x;
-			min.y /= screenSize.y;
-			max.x /= screenSize.x;
-			max.y /= screenSize.y;
-
-			rect.anchorMin = min;
-			rect.anchorMax = max;
+			rect.anchorMin = Vector2.zero;
+			rect.anchorMax = Vector2.one;
+			rect.offsetMin = new Vector2(safeArea.position.x, safeArea.position.y);
+			rect.offsetMax = new Vector2(-(screenSize.x - (safeArea.position.x + safeArea.width)), -(screenSize.y - (safeArea.position.y + safeArea.height)));
 
 			return rect;
 		}
---
+++ Assets/Flour/Scripts/Layer/SafeAreaSimulateData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flour.Layer
+{
+	internal static class SafeAreaSimulateData
+	{
+#if UNITY_EDITOR
+		static readonly Vector2[] Resolutions = new Vector2[]
+		{
+			new Vector2(1125, 2436),
+			new Vector2(828, 1792),
+			new Vector2(1242, 2688),
+			new Vector2(1668, 2388),
+			new Vector2(2048, 2732),
+		};
+
+		static readonly Rect[,] SafeAreaResolutions = new Rect[,]
+		{
+			{ new Rect(0, 102, 1125, 2202), new Rect(132, 63, 2172, 1062) },
+			{ new Rect(0, 68, 828, 1636), new Rect(88, 42, 1616, 786) },
+			{ new Rect(0, 102, 1242, 2454), new Rect(132, 63, 2424, 1179) },
+			{ new Rect(0, 40, 1668, 2348), new Rect(0, 40, 2388, 1628) },
+			{ new Rect(0, 40, 2048, 2692), new Rect(0, 40, 2732, 2008) },
+		};
+#endif
+
+		internal static Rect GetSafeArea(Vector2 screenSize, Rect safeArea)
+		{
+#if UNITY_EDITOR
+			for (int i = 0; i < Resolutions.Length; i++)
+			{
+				var resolution = Resolutions[i];
+				var portrait = screenSize.x == resolution.x && screenSize.y == resolution.y;
+				var landscape = screenSize.x == resolution.y && screenSize.y == resolution.x;
+
+				if (portrait || landscape)
+				{
+					return SafeAreaResolutions[i, portrait ? 0 : 1];
+				}
+			}
+#endif
+			return safeArea;
+		}
+	}
+}
