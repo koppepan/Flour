0a41aeaf0db5e459b3c1cf8e2ceb22472a5b7aa5
koppepan <koppepan0114@gmail.com>
2019-05-27 17:05:31 +0900

    SaveDataのKeyをEnumで管理できるようのwrapper

---
+++ Assets/Example/Scripts/SaveData.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+public class SaveData : System.IDisposable
+{
+	public enum Key
+	{
+	}
+
+	readonly Flour.UserPrefs userPrefs;
+
+	public SaveData() => userPrefs = new Flour.UserPrefs();
+	public SaveData(string userId) => userPrefs = new Flour.UserPrefs(userId);
+	public void Dispose() => userPrefs.Dispose();
+
+	public void DeleteUser(string userId)
+	{
+		var keys = System.Enum.GetValues(typeof(Key)).Cast<Key>();
+		foreach (var key in keys)
+		{
+			DeleteKey(key);
+		}
+	}
+	public void DeleteKey(Key key) => userPrefs.DeleteKey(key.ToString());
+
+	public void SetInt(Key key, int value) => userPrefs.SetInt(key.ToString(), value);
+	public void SetFloat(Key key, int value) => userPrefs.SetFloat(key.ToString(), value);
+	public void SetString(Key key, string value) => userPrefs.SetString(key.ToString(), value);
+	public void SetValue<T>(Key key, T value) => userPrefs.SetValue<T>(key.ToString(), value);
+
+	public int GetInt(Key key, int defautValue = 0) => userPrefs.GetInt(key.ToString(), defautValue);
+	public float GetFloat(Key key, float defautValue = 0) => userPrefs.GetFloat(key.ToString(), defautValue);
+	public string GetString(Key key, string defautValue = "") => userPrefs.GetString(key.ToString(), defautValue);
+	public T GetValue<T>(Key key) => userPrefs.GetValue<T>(key.ToString());
+}
