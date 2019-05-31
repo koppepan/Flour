e772c5a329d410534d3cefca66851d5a2e14051d
koppepan <koppepan0114@gmail.com>
2019-05-31 17:37:55 +0900

    DefineSymbolsの定義を設定する処理

---
+++ Assets/Flour/Editor/Build/BuildScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+namespace Flour.Build
+{
+	public static class BuildScript
+	{
+		public static void SetDefineSynboles(BuildTargetGroup targetGroup, IEnumerable<string> addSymbols, IEnumerable<string> removeSymbols)
+		{
+			var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup).Split(';').Where(x => !string.IsNullOrEmpty(x));
+
+			symbols = symbols.Where(x => !removeSymbols.Any(y => x == y));
+			symbols = symbols.Concat(addSymbols);
+
+			var joinSymbols = string.Join(";", symbols.OrderBy(x => x).Distinct());
+			PlayerSettings.SetScriptingDefineSymbolsForGroup(targetGroup, joinSymbols);
+
+			Debug.Log($"apply define symbols => \"{joinSymbols}\"");
+			AssetDatabase.SaveAssets();
+		}
+	}
+}
