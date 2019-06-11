cc0092aa5311cca6e2f4f712378893bc6ce37849
koppepan <koppepan0114@gmail.com>
2019-06-11 18:42:54 +0900

    ScrollのSample

---
+++ Assets/Example/Scripts/Layer/Component/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Example
+{
+	public class Test : MonoBehaviour, Flour.UI.IListItem
+	{
+		[SerializeField]
+		Text text = default;
+
+		RectTransform cache;
+		public RectTransform RectTransform
+		{
+			get
+			{
+				if (cache == null) cache = GetComponent<RectTransform>();
+				return cache;
+			}
+		}
+
+		Func<int, string> getElement;
+
+		public void Initilize(Func<int, string> getElement)
+		{
+			this.getElement = getElement;
+		}
+
+		public void UpdateData(int index)
+		{
+			text.text = getElement(index);
+		}
+	}
+}
---
+++ Assets/Example/Scripts/Layer/Component/TestList.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+
+namespace Example
+{
+	[RequireComponent(typeof(ScrollRect))]
+	public class TestList : MonoBehaviour
+	{
+		[SerializeField]
+		Test prefab = default;
+
+		ScrollRect scrollRect;
+		Flour.UI.ListLayoutGroup layoutGroup;
+
+		List<string> source = new List<string>();
+
+		private void Awake()
+		{
+			Assert.IsNotNull(GetComponent<ScrollRect>(), "not found ScrollRect component.");
+			scrollRect = GetComponent<ScrollRect>();
+
+			Assert.IsNotNull(scrollRect.content.GetComponent<Flour.UI.ListLayoutGroup>(), "LayoutGroup is not attached to content.");
+			layoutGroup = scrollRect.content.GetComponent<Flour.UI.ListLayoutGroup>();
+
+			for (int i = 0; i < 20; i++)
+			{
+				source.Add(i.ToString());
+			}
+
+			prefab.gameObject.SetActive(false);
+			layoutGroup.Initialize(scrollRect.horizontal, scrollRect.vertical, scrollRect.viewport, source.Count, prefab.RectTransform.sizeDelta, Create);
+		}
+
+		Flour.UI.IListItem Create()
+		{
+			var item = Instantiate(prefab);
+			item.Initilize(index => source[index]);
+			return item;
+		}
+	}
+}
