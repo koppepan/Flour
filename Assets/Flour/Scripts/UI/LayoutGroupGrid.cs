d048040e4bbd8b359cefa2331018843c534479d1
koppepan <koppepan0114@gmail.com>
2019-06-12 17:33:51 +0900

    Class名間違ってたの修正
    padding.rightを考慮

--- Assets/Flour/Scripts/UI/LayoutGroupGrid.cs
+++ Assets/Flour/Scripts/UI/LayoutGroupGrid.cs
@@ -2,24 +2,25 @@
 
 namespace Flour.UI
 {
-	public class LayoutGruopGrid : LayoutGroup
+	public class LayoutGroupGrid : LayoutGroup
 	{
 		protected override void SetLocalPosition(int elementCount, Vector2 elementSize)
 		{
 			Vector2 startPos, pos;
 			startPos = pos = new Vector2(padding.left, -padding.top);
 
-			for (int i = 0; i < elementCount; i++)
-			{
-				localPositionCache[i] = new Rect(pos, elementSize);
+			var p = new Vector2(padding.right, padding.bottom);
+			localPositionCache[0] = new Rect(pos, elementSize);
 
+			for (int i = 1; i < elementCount; i++)
+			{
 				switch (scroll)
 				{
 					case Scroll.Horizontal:
 
 						pos.y -= elementSize.y + spacing;
 
-						if (RectTransform.TransformPoint(pos + elementSize).y > LimitRect.max.y)
+						if (RectTransform.TransformPoint(pos + elementSize + p).y > LimitRect.max.y)
 						{
 							pos.y = startPos.y;
 							pos.x += elementSize.x + spacing;
@@ -30,13 +31,14 @@ namespace Flour.UI
 					case Scroll.Vertical:
 						pos.x += elementSize.x + spacing;
 
-						if (RectTransform.TransformPoint(pos + elementSize).x > LimitRect.max.x)
+						if (RectTransform.TransformPoint(pos + elementSize + p).x > LimitRect.max.x)
 						{
 							pos.x = startPos.x;
 							pos.y -= elementSize.y + spacing;
 						}
 						break;
 				}
+				localPositionCache[i] = new Rect(pos, elementSize);
 			}
 		}
 	}
