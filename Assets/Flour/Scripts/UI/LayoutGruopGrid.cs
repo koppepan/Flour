e2e805375fd4b1946dbfec0b1e3c2384e3fd9a13
koppepan <koppepan0114@gmail.com>
2019-06-12 14:53:17 +0900

    Gird用のLayoutGroup

---
+++ Assets/Flour/Scripts/UI/LayoutGruopGrid.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Flour.UI
+{
+	public class LayoutGruopGrid : LayoutGroup
+	{
+		protected override void SetLocalPosition(int elementCount, Vector2 elementSize)
+		{
+			Vector2 startPos, pos;
+			startPos = pos = new Vector2(padding.left, -padding.top);
+
+			for (int i = 0; i < elementCount; i++)
+			{
+				localPositionCache[i] = new Rect(pos, elementSize);
+
+				switch (scroll)
+				{
+					case Scroll.Horizontal:
+
+						pos.y -= elementSize.y + spacing;
+
+						if (RectTransform.TransformPoint(pos + elementSize).y > LimitRect.max.y)
+						{
+							pos.y = startPos.y;
+							pos.x += elementSize.x + spacing;
+						}
+
+						break;
+
+					case Scroll.Vertical:
+						pos.x += elementSize.x + spacing;
+
+						if (RectTransform.TransformPoint(pos + elementSize).x > LimitRect.max.x)
+						{
+							pos.x = startPos.x;
+							pos.y -= elementSize.y + spacing;
+						}
+						break;
+				}
+			}
+		}
+	}
+}
