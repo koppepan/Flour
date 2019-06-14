d9d096dd63eedbde9e19c9de44473631f53a6bd4
koppepan <koppepan0114@gmail.com>
2019-06-14 15:06:54 +0900

    AssetBundleを並列でDownloadするやつ

---
+++ Assets/Flour/Scripts/Asset/AssetBundleDownloader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Flour.Asset
+{
+	public class ParallelAssetBundleDownloader : Net.ParallelWebRequest<AssetBundle>
+	{
+		public ParallelAssetBundleDownloader(string baseUrl, int parallel, int timeout, IObserver<Tuple<string, AssetBundle>> downloadObserver, IObserver<Tuple<string, long>> errorObserver)
+			: base(baseUrl, parallel, timeout, downloadObserver, errorObserver)
+		{
+		}
+	}
+
+	public struct AssetBundleDownloader : Net.IDownloader<AssetBundle>
+	{
+		public string Path { get; private set; }
+		public UnityWebRequest Request { get; private set; }
+
+		Hash128 hash;
+
+		public AssetBundleDownloader(string path, Hash128 hash)
+		{
+			Path = path;
+			Request = null;
+			this.hash = hash;
+		}
+
+		public void Send(string baseUrl, int timeout)
+		{
+			Request = UnityWebRequestAssetBundle.GetAssetBundle(System.IO.Path.Combine(baseUrl, Path), hash);
+			Request.timeout = timeout;
+			Request.SendWebRequest();
+		}
+		public AssetBundle GetContent()
+		{
+			return DownloadHandlerAssetBundle.GetContent(Request);
+		}
+		public void Dispose()
+		{
+			Request.Dispose();
+		}
+	}
+}
