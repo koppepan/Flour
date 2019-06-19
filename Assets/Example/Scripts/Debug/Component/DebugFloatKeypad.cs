b18ba3994f20d529340ce10b91e60b09a52e8cae
koppepan <koppepan0114@gmail.com>
2019-06-19 11:44:09 +0900

    update debug prefab

---
+++ Assets/Example/Scripts/Debug/Component/DebugFloatKeypad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example
+{
+	class DebugFloatKeypad : DebugKeypad, DebugDialog.IContent<double>
+	{
+		public void Setup(Action<double> onSelect)
+		{
+			SetupFloat(onSelect);
+		}
+		public double GetValue()
+		{
+			return double.Parse(input);
+		}
+	}
+}
---
+++ Assets/Example/Scripts/Debug/Component/DebugIntegerKeypad.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example
+{
+	class DebugIntegerKeypad : DebugKeypad, DebugDialog.IContent<long>
+	{
+		public void Setup(Action<long> onSelect)
+		{
+			SetupInteger(onSelect);
+		}
+		public long GetValue()
+		{
+			return long.Parse(input);
+		}
+	}
+}
