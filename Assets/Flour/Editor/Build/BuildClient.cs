a2c6387a7d1501bc33175a303f9099f62f767107
koppepan <koppepan0114@gmail.com>
2019-07-01 18:16:19 +0900

    雑にClientBuild処理を作成

--- Assets/Flour/Editor/Build/BuildClient.cs
+++ Assets/Flour/Editor/Build/BuildClient.cs
@@ -4,12 +4,159 @@ using System.Linq;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using Flour.Config;
 
 namespace Flour.Build
 {
 	public static class BuildClient
 	{
+		public struct PlayerBuildConfig
+		{
+			public string productName;
+			public string bundleVersion;
+			public int buildNumber;
+			public string outputPath;
+			public BuildTargetGroup targetGroup;
+			public BuildTarget buildTarget;
+			public BuildOptions options;
+			public List<ConnectInfomation> connectInfomations;
+		}
+
+		public static void Build()
+		{
+			var command = Environment.CommandLine;
+			var args = command.Split(' ');
+
+			var config = new PlayerBuildConfig
+			{
+				options = BuildOptions.None,
+				connectInfomations = GetServerList("Development"),
+			};
+
+			for (int i = 0; i < args.Length; i++)
+			{
+				switch (args[i])
+				{
+					case "ProductName":
+						config.productName = args[i + 1];
+						i++;
+						break;
+
+					case "BundleVersion":
+						config.bundleVersion = args[i + 1];
+						i++;
+						break;
+
+					case "BuildNumber":
+						config.buildNumber = int.Parse(args[i + 1]);
+						i++;
+						break;
+
+					case "OutputPath":
+						config.outputPath = args[i + 1];
+						i++;
+						break;
+
+					case "TargetGroup":
+						config.targetGroup = (BuildTargetGroup)Enum.Parse(typeof(BuildTargetGroup), args[i + 1]);
+						i++;
+						break;
+
+					case "BuildTarget":
+						config.buildTarget = (BuildTarget)Enum.Parse(typeof(BuildTarget), args[i + 1]);
+						i++;
+						break;
+				}
+			}
+			Build(config);
+		}
+
+		public static void Build(PlayerBuildConfig config)
+		{
+			PlayerSettings.productName = config.productName;
+			PlayerSettings.bundleVersion = config.bundleVersion;
+
+			if (config.buildTarget == BuildTarget.iOS)
+			{
+				PlayerSettings.iOS.buildNumber = config.buildNumber.ToString();
+			}
+			else if (config.buildTarget == BuildTarget.Android)
+			{
+				PlayerSettings.Android.bundleVersionCode = config.buildNumber;
+			}
+
+			var connectInfo = Resources.Load<ServerList>("Config/ServerList");
+			connectInfo.list = config.connectInfomations;
+
+			if (config.buildTarget != EditorUserBuildSettings.activeBuildTarget)
+			{
+				EditorUserBuildSettings.SwitchActiveBuildTarget(config.targetGroup, config.buildTarget);
+			}
+
+			var report = BuildPipeline.BuildPlayer(new BuildPlayerOptions
+			{
+				scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray(),
+				locationPathName = config.outputPath,
+				target = config.buildTarget,
+				targetGroup = config.targetGroup,
+				options = config.options,
+			});
+
+			if (report.summary.result == BuildResult.Succeeded)
+			{
+				Debug.Log("Build succeeded: " + report.summary.totalSize + " bytes");
+			}
+			else if (report.summary.result == BuildResult.Failed)
+			{
+				Debug.Log("Build failed");
+			}
+		}
+
+		[MenuItem("Flour/Build/Production")]
+		public static void BuildProduction()
+		{
+			Build(new PlayerBuildConfig
+			{
+				productName = "production",
+				bundleVersion = "1.0.0",
+				buildNumber = 1,
+				outputPath = "Builds/Production/Production.exe",
+				targetGroup = BuildTargetGroup.Standalone,
+				buildTarget = BuildTarget.StandaloneWindows64,
+				connectInfomations = GetServerList("Production"),
+				options = BuildOptions.None,
+			});
+		}
+
+		[MenuItem("Flour/Build/Development")]
+		public static void BuildDevelopment()
+		{
+			Build(new PlayerBuildConfig
+			{
+				productName = "development",
+				bundleVersion = "1.0.0",
+				buildNumber = 1,
+				outputPath = "Builds/Development/Development.exe",
+				targetGroup = BuildTargetGroup.Standalone,
+				buildTarget = BuildTarget.StandaloneWindows64,
+				connectInfomations = GetServerList("Development"),
+				options = BuildOptions.Development,
+			});
+		}
+
+		private static List<ConnectInfomation> GetServerList(string key)
+		{
+			var ini = new IniFile(Path.Combine(Application.dataPath, $"../BuildConfig/{key}/ConnectInformation.ini"));
+
+			return ini.GetSections()
+				.Where(x => !string.IsNullOrEmpty(x))
+				.Select(x =>
+				{
+					return new ConnectInfomation { name = x, api = ini.GetValue(x, "API"), assetBundle = ini.GetValue(x, "AssetBundle") };
+				}).ToList();
+		}
+
 		public static void SetDefineSynboles(BuildTargetGroup targetGroup, IEnumerable<string> addSymbols, IEnumerable<string> removeSymbols)
 		{
 			var symbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetGroup).Split(';').Where(x => !string.IsNullOrEmpty(x));
@@ -22,41 +169,6 @@ namespace Flour.Build
 
 			Debug.Log($"apply define symbols => \"{joinSymbols}\"");
 			AssetDatabase.SaveAssets();
-		}
-
-		public static void Build()
-		{
-		}
-
-		[MenuItem("Flour/Build/Production")]
-		public static void BuildProduction()
-		{
-			ApplyServerList("Production");
-		}
-
-		[MenuItem("Flour/Build/Development")]
-		public static void BuildDevelopment()
-		{
-			ApplyServerList("Development");
-		}
-
-		private static void ApplyServerList(string key)
-		{
-			var ini = new IniFile(Path.Combine(Application.dataPath, $"../BuildConfig/{key}/ConnectInformation.ini"));
-
-			var connectInfo = Resources.Load<ServerList>("Config/ServerList");
-			connectInfo.list.Clear();
-
-			foreach (var section in ini.GetSections())
-			{
-				if (string.IsNullOrEmpty(section)) continue;
-				connectInfo.list.Add(new ConnectInfomation
-				{
-					name = section,
-					api = ini.GetValue(section, "API"),
-					assetBundle = ini.GetValue(section, "AssetBundle"),
-				});
-			}
 		}
 	}
 }
