1ab6473dceb035ac2393d8009fc53cc93fff6629
koppepan <koppepan0114@gmail.com>
2019-07-01 17:01:14 +0900

    起動時にLoadする接続先情報を設定するScriptableObject

---
+++ Assets/Flour/Scripts/Config/ServerList.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Flour.Config
+{
+	[System.Serializable]
+	public struct ConnectInfomation
+	{
+		public string name;
+		public string api;
+		public string assetBundle;
+	}
+
+	[CreateAssetMenu()]
+	public class ServerList : ScriptableObject
+	{
+		public List<ConnectInfomation> list = new List<ConnectInfomation>();
+	}
+}
