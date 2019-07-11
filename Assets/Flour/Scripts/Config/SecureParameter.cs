980ffcce98577b915a3a23c8e2b23914d584d1ba
koppepan <koppepan0114@gmail.com>
2019-07-11 13:09:39 +0900

    AssetBundle複合用のPasswordを保持するScriptableObject

---
+++ Assets/Flour/Scripts/Config/SecureParameter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Flour.Config
+{
+	[CreateAssetMenu()]
+	public class SecureParameter : ScriptableObject
+	{
+		public string Password { get { return _password; } }
+
+		[SerializeField]
+		private string _password = default;
+	}
+}
