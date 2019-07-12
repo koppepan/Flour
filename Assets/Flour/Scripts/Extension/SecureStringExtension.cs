52bf2c41e015ca977e8fd8f2227187276e036f00
koppepan <koppepan0114@gmail.com>
2019-07-12 12:59:24 +0900

    SecureStringの拡張

---
+++ Assets/Flour/Scripts/Extension/SecureStringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+using System.Security;
+
+namespace Flour
+{
+	public static class SecureStringExtension
+	{
+		public static SecureString Set(this SecureString secure, string plainText)
+		{
+			for (int i = 0; i < plainText.Length; i++) secure.AppendChar(plainText[i]);
+			return secure;
+		}
+		public static string ToPlainText(this SecureString secure)
+		{
+			return Marshal.PtrToStringUni(Marshal.SecureStringToGlobalAllocUnicode(secure));
+		}
+	}
+}
