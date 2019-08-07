b4d0be7ffc0e91664efcce3f7c13a7e0da3dd84a
koppepan <koppepan0114@gmail.com>
2019-08-07 10:14:10 +0900

    処理でどれだけのメモリを使っているかを検知する処理

---
+++ Assets/Flour/Scripts/Develop/AppDomainMonitorDelta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Flour.Develop
+{
+	public sealed class AppDomainMonitorDelta : IDisposable
+	{
+
+		private readonly AppDomain appDomain;
+		private readonly TimeSpan thisADCpu;
+		private readonly long thisADMemoryInUse;
+		private readonly long thisADMemoryAllocated;
+
+		static AppDomainMonitorDelta()
+		{
+			AppDomain.MonitoringIsEnabled = true;
+		}
+
+		public AppDomainMonitorDelta(AppDomain domain)
+		{
+			appDomain = domain ?? AppDomain.CurrentDomain;
+			thisADCpu = appDomain.MonitoringTotalProcessorTime;
+			thisADMemoryInUse = appDomain.MonitoringSurvivedMemorySize;
+			thisADMemoryAllocated = appDomain.MonitoringTotalAllocatedMemorySize;
+		}
+
+		public void Dispose()
+		{
+			GC.Collect();
+
+			Debug.LogFormat("FriendlyName={0}, CPU={1}ms", appDomain.FriendlyName, appDomain.MonitoringTotalProcessorTime - thisADCpu);
+			Debug.LogFormat("  Allocated {0:N0} bytes of which {1:N0} survived GCs",
+				appDomain.MonitoringTotalAllocatedMemorySize - thisADMemoryAllocated,
+				appDomain.MonitoringSurvivedMemorySize - thisADMemoryInUse);
+		}
+	}
+}
