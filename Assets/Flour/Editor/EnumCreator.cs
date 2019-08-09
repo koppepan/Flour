21261a99f50f8d4cbaf0d5514eb64bbd209b2d8b
koppepan <koppepan0114@gmail.com>
2019-08-09 18:09:23 +0900

    EnumとScene遷移のコード生成をFileWriterに置き換え

--- Assets/Flour/Editor/EnumCreator.cs
+++ Assets/Flour/Editor/EnumCreator.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.IO;
 using UnityEngine;
-using UnityEditor;
 
 namespace Flour
 {
@@ -11,9 +9,9 @@ namespace Flour
 	/// </summary>
 	public static class EnumCreator
 	{
-		public static void Create(string exportPath, string nameSpace, string summary, string enumName, IEnumerable<string> types)
+		public static void Create(string exportDirectory, string nameSpace, string summary, string enumName, IEnumerable<string> types)
 		{
-			if (string.IsNullOrEmpty(exportPath))
+			if (string.IsNullOrEmpty(exportDirectory) || !Directory.Exists(exportDirectory))
 			{
 				Debug.LogError("export path empty.");
 				return;
@@ -24,55 +22,21 @@ namespace Flour
 				return;
 			}
 
-			string tab = "";
-
-			using (var sw = File.CreateText(Path.Combine(exportPath, enumName + ".cs")))
+			using (var fw = new FileWriter(exportDirectory, enumName + ".cs"))
 			{
-				if (!string.IsNullOrEmpty(nameSpace))
+				fw.WriteSummary(summary);
+
+				using (fw.StartNamespaceScope(nameSpace))
 				{
-					sw.WriteLine($"namespace {nameSpace}");
-					sw.WriteLine("{");
-
-					AddTab(ref tab);
-				}
-
-				if (!string.IsNullOrEmpty(summary))
-				{
-					sw.WriteLine(tab + "/// <summary>");
-					sw.WriteLine(tab + $"/// {summary}");
-					sw.WriteLine(tab + "/// </summary>");
-				}
-
-				sw.WriteLine(tab + $"public enum {enumName}");
-				sw.WriteLine(tab + "{");
-
-				AddTab(ref tab);
-				foreach (var type in types)
-				{
-					sw.WriteLine(tab + type + ",");
-				}
-				RemoveTab(ref tab);
-				sw.WriteLine(tab + "}");
-
-				if (!string.IsNullOrEmpty(nameSpace))
-				{
-					RemoveTab(ref tab);
-					sw.WriteLine(tab + "}");
+					using (fw.StartEnumScope(enumName))
+					{
+						foreach (var t in types)
+						{
+							fw.WriteBody(t + ",");
+						}
+					}
 				}
 			}
-
-			var path = Path.Combine(exportPath, enumName + ".cs");
-			AssetDatabase.ImportAsset(path);
-			Debug.Log("created " + path);
-		}
-
-		static void AddTab(ref string tab)
-		{
-			tab += "\t";
-		}
-		static void RemoveTab(ref string tab)
-		{
-			tab = tab.Remove(0, "\t".Length);
 		}
 	}
 }
--- Assets/Flour/Editor/SceneListCreator.cs
+++ Assets/Flour/Editor/SceneListCreator.cs
@@ -7,91 +7,60 @@ namespace Flour
 {
 	public static class SceneListCreator
 	{
+		const string SceneListClass = "SceneList";
+
 		public static void Create(string sceneDirectory, string outputDirectory, string menuTitle, int priority, string namespaceName = "")
 		{
 			string scriptName = "SceneList.cs";
 
-			string[] sceneDirs = { Path.Combine("Assets", sceneDirectory) };
+			string[] sceneDirs = { sceneDirectory };
 			if (!Directory.Exists(sceneDirs[0]))
 			{
 				Debug.LogError($"not exists scene directory => {sceneDirs[0]}");
 				return;
 			}
 
-			string scriptFilePath = Path.Combine(Application.dataPath, outputDirectory, scriptName);
+			string scriptFilePath = Path.Combine(Application.dataPath.Replace("Assets", ""), outputDirectory, scriptName);
 			if (!Directory.Exists(Path.GetDirectoryName(scriptFilePath)))
 			{
 				Debug.LogError($"not exists output directory => {Path.GetDirectoryName(scriptFilePath)}");
 				return;
 			}
 
-			string scriptAssetPath = Path.Combine("Assets", outputDirectory, scriptName);
+			string scriptAssetPath = Path.Combine(outputDirectory, scriptName);
 
 			string[] sceneGUIDs = AssetDatabase.FindAssets("t:Scene", sceneDirs);
-			string tab = "";
 
-			using (StreamWriter sw = File.CreateText(scriptFilePath))
+			using (var fw = new FileWriter(outputDirectory, SceneListClass + ".cs"))
 			{
-				sw.WriteLine("using UnityEditor;");
-				sw.WriteLine("using UnityEditor.SceneManagement;");
-				sw.WriteLine("");
+				fw.WriteUsing("UnityEditor");
+				fw.WriteUsing("UnityEditor.SceneManagement");
+				fw.WriteLine();
 
-				if (!string.IsNullOrEmpty(namespaceName))
+				using (fw.StartNamespaceScope(namespaceName))
 				{
-					sw.WriteLine($"namespace {namespaceName}");
-					sw.WriteLine("{");
-					AddTab(ref tab);
-				}
+					using (fw.StartClassScope(SceneListClass))
+					{
+						foreach (var sceneGUID in sceneGUIDs)
+						{
+							string scenePath = AssetDatabase.GUIDToAssetPath(sceneGUID);
+							string sceneName = scenePath.Remove(0, (sceneDirectory + "/").Length).Replace(".unity", "");
+							int depth = sceneName.Count(x => x == '/');
 
-				sw.WriteLine(tab + "public class SceneList");
-				sw.WriteLine(tab + "{");
-				AddTab(ref tab);
-
-				foreach (var sceneGUID in sceneGUIDs)
-				{
-					string scenePath = AssetDatabase.GUIDToAssetPath(sceneGUID);
-					string sceneName = scenePath.Remove(0, ("Assets/" + sceneDirectory + "/").Length).Replace(".unity", "");// splittedScenePath[splittedScenePath.Length - 1].Replace(".unity", "");
-					int depth = sceneName.Count(x => x == '/');
-
-					sw.WriteLine(tab + $"[MenuItem(\"{menuTitle}/Scene/{sceneName}\", priority = {priority + depth})]");
-					sw.WriteLine(tab + $"public static void OpenScene{sceneName.Replace("/", "")}()");
-					sw.WriteLine(tab + "{");
-
-					AddTab(ref tab);
-
-					sw.WriteLine(tab + "if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())");
-					sw.WriteLine(tab + "{");
-
-					AddTab(ref tab);
-
-					sw.WriteLine(tab + $"EditorSceneManager.OpenScene(\"{scenePath}\");");
-
-					RemoveTab(ref tab);
-
-					sw.WriteLine(tab + "}");
-					RemoveTab(ref tab);
-					sw.WriteLine(tab + "}");
-				}
-				RemoveTab(ref tab);
-				sw.WriteLine(tab + "}");
-
-				if (!string.IsNullOrEmpty(namespaceName))
-				{
-					RemoveTab(ref tab);
-					sw.WriteLine(tab + "}");
+							fw.WriteBody($"[MenuItem(\"{menuTitle}/Scene/{sceneName}\", priority = {priority + depth})]");
+							fw.WriteBody($"public static void OpenScene{sceneName.Replace("/", "")}()");
+							using (fw.StartScope())
+							{
+								fw.WriteBody("if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())");
+								using (fw.StartScope())
+								{
+									fw.WriteBody($"EditorSceneManager.OpenScene(\"{scenePath}\");");
+								}
+							}
+						}
+					}
 				}
 			}
-			AssetDatabase.ImportAsset(scriptAssetPath);
-			Debug.Log($"created {scriptName}");
-		}
-
-		static void AddTab(ref string tab)
-		{
-			tab += "\t";
-		}
-		static void RemoveTab(ref string tab)
-		{
-			tab = tab.Remove(0, "\t".Length);
 		}
 	}
 }
